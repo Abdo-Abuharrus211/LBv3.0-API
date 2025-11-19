af2b34f4dd6cafd2b6765342498e28061b5156cf
Abdo-Abuharrus211 <aabuharrus@gmail.com>
2025-11-18 21:44:47 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/get-questions')
+def get_questions():
+    return ""
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/questions.py
@@ -0,0 +1,22 @@
+"""
+    This class conducts operations for all the questions.
+"""
+
+
+class Questions:
+    def __init__(self):
+        pass
+
+    def get_questions_from_db(self):
+        pass
+
+    def get_question_answers_from_db(self):
+        pass
+
+    """
+    Pick 10 random from the db pool.
+    @:return JSON string of the questions and their data 
+    """
+
+    def randomize_questions(self):
+        pass
