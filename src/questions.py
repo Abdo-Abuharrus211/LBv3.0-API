bc52a3a79b943d9b8fc15c0ad896922f4b8ea16c
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-11-22 15:17:13 -0800

    Add docstrings to questions.py

--- src/questions.py
+++ src/questions.py
@@ -8,15 +8,24 @@ class Questions:
         pass
 
     def get_questions_from_db(self):
+        """
+        Fetch the questions from the database.
+
+        :return: Dictionary of the questions, keys are question ids and values are JSON strings
+        """
         pass
 
     def get_question_answers_from_db(self):
+        """
+        Fetch the answers from the database.
+
+        :return: Dictionary of the answers, keys are answer ids and values are JSON strings
+        """
         pass
 
-    """
-    Pick 10 random from the db pool.
-    @:return JSON string of the questions and their data 
-    """
-
     def randomize_questions(self):
+        """
+            Pick 10 random from the db pool.
+            @:return JSON string of the questions and their data
+        """
         pass
